8a5fef6a5f24d3a01a4e4c4acea9386ad7f33f8b
SUNGIN99 <90676644+SUNGIN99@users.noreply.github.com>
2025-03-21 23:51:36 +0900

    Title: 산타의 선물 공장 2, Time: 443ms, Memory: 25MB, Status: Runtime Error - CodeTree

--- 250321/산타의 선물 공장 2/santa-gift-factory-2.java
+++ 250321/산타의 선물 공장 2/santa-gift-factory-2.java
@@ -21,6 +21,7 @@ public class Main {
 
     static Stuck[] beltHead, beltRear;
     static int[] stuckCount;
+    static HashMap<Integer, Stuck> map = new HashMap<>();
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -56,6 +57,8 @@ public class Main {
                     Stuck stuck = new Stuck(i+1);
                     int beltNum = Integer.parseInt(st.nextToken());
 
+                    map.put(i+1, stuck);
+
                     Stuck back = beltRear[beltNum];
                     back.addFront(stuck);
                     stuckCount[beltNum] ++;
@@ -89,6 +92,8 @@ public class Main {
 
                 beltHead[to].right = first;
                 first.left= beltHead[to];
+
+                System.out.println(stuckCount[to]);
             }else if(op == 300){
                 int from = Integer.parseInt(st.nextToken());
                 int to = Integer.parseInt(st.nextToken());
@@ -111,7 +116,9 @@ public class Main {
                     dstFirst.right.left = beltHead[to];
 
                     srcFirst.addFront(dstFirst);
-                    
+
+                    stuckCount[from] ++;
+                    stuckCount[to] --;
                 }else{
                     beltHead[from].right = srcFirst.right;
                     srcFirst.right.left = beltHead[from];
@@ -122,14 +129,75 @@ public class Main {
                     beltHead[from].right.addFront(dstFirst);
                     beltHead[to].right.addFront(srcFirst);
                 }
+
+                System.out.println(stuckCount[to]);
             }else if(op == 400){
                 int from = Integer.parseInt(st.nextToken());
                 int to = Integer.parseInt(st.nextToken());
 
+                if(stuckCount[from] == 1){
+                    continue;
+                }
+
+                int max = stuckCount[from] / 2;
+                Stuck cur = beltHead[from];
+                for(int i = 0; i< max; i++){
+                    cur = cur.right;
+                }
+                
+                Stuck srcFirst = beltHead[from].right;
+                Stuck dstFirst = beltHead[to].right;
+                
+                beltHead[to].right = srcFirst;
+                srcFirst.left = beltHead[to];
+
+                beltHead[from].right = cur.right;
+                cur.right.left = beltHead[from];
+                
+                dstFirst.left = cur;
+                cur.right = dstFirst;
+
+                stuckCount[from] -= max;
+                stuckCount[to] += max;
+
+                System.out.println(stuckCount[to]);
+                /*
+                for(int i = 1; i<=n; i++){
+                    Stuck cur2 = beltHead[i];
+                    while(cur2.right != null){
+                        System.out.print(cur2.num + ", " );
+                        cur2 = cur2.right;
+                    }
+                    System.out.println();
+                }
+                */
+
             }else if(op == 500){
+                int pnum = Integer.parseInt(st.nextToken());
+
+                Stuck stuck = map.get(pnum);
+                int a = -1, b = -1;
+                if(!stuck.left.equals(null)){
+                    a = stuck.left.num;
+                }
+                if(!stuck.right.equals(null)){
+                    b = stuck.right.num;
+                }
+
+                System.out.println(a + 2* b);
 
             }else if(op == 600){
+                int pnum = Integer.parseInt(st.nextToken());
 
+                int a = -1, b= -1, c = stuckCount[pnum];
+                if(!beltHead[pnum].right.equals(null)){
+                    a = beltHead[pnum].right.num;
+                }
+                if(!beltRear[pnum].left.equals(null)){
+                    b= beltRear[pnum].left.num;
+                }
+
+                System.out.println(a + 2*b + 3*c);
             }
         }
     }
