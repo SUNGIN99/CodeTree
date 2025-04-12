b898e655ecb4e1e5e9e2b4e30660a210f8e505c2
SUNGIN99 <90676644+SUNGIN99@users.noreply.github.com>
2025-04-12 16:48:37 +0900

    Title: 메두사와 전사들, Time: 92ms, Memory: 9MB, Status: Wrong Answer - Codetree

--- 250412/메두사와 전사들/medusa-and-warriors.java
+++ 250412/메두사와 전사들/medusa-and-warriors.java
@@ -78,22 +78,162 @@ public class Main {
             }
 
             String[] dir = route.split("");
+            int pcount = 0;
             for(String d : dir){
                 medusaMove(d);
+                if(pcount == dir.length - 1){
+                    
+                    break;
+                }
 
                 medusaLook();
-                break;
+
+                warriorMove();
+                pcount++;
+
+               // break;
             }
         }
     }
+    static int movedWarriorCount, stonedWarriorCount, attackedWarriorCount;
+    static void warriorMove(){
+        movedWarriorCount = stonedWarriorCount = attackedWarriorCount = 0;
 
-    static int[][] medusaSeeU, medusaSeeD, medusaSeeL, medusaSeeR
+        ArrayList<Warrior>[][] newWarrior = new ArrayList[n][];
+        for(int i = 0; i<n; i++){
+            newWarrior[i] = new ArrayList[n];
+            for(int j = 0; j<n; j++){
+                newWarrior[i][j] = new ArrayList<>();
+            }
+            //System.out.println(Arrays.toString(stonedArea[i]));
+        }
+        
+
+        for(int i = 0; i<n; i++){
+            for(int j = 0; j<n; j++){
+                if(stonedWarrior.contains(i + " " + j)){
+                    stonedWarriorCount += warrior[i][j].size();
+                    newWarrior[i][j].addAll(warrior[i][j]);
+                }else{
+                    if(warrior[i][j].size() > 0){
+                        int[] first = firstMove(i, j);
+                        //System.out.println(i+" " + j + " -> " + Arrays.toString(first));
+                        if(first[0] == sr && first[1] == sc){
+                            attackedWarriorCount += warrior[i][j].size();
+                            movedWarriorCount += warrior[i][j].size();
+                            continue;
+                        }
+
+                        int[] second = secondMove(first[0], first[1]);
+                        //System.out.println(Arrays.toString(second) + "\n");
+
+                        int dist2 = Math.abs(i - second[0]) + Math.abs(j - second[1]);
+
+                        if(first[2] < second[2]){
+                            movedWarriorCount += warrior[i][j].size();
+                            newWarrior[first[0]][first[1]].addAll(warrior[i][j]);
+                            continue;
+                        }else{
+                            movedWarriorCount += warrior[i][j].size() * dist2;
+                        }
+
+                        if(second[0] == sr && second[1] == sc){
+                            attackedWarriorCount += warrior[i][j].size();
+                        }else{
+                            newWarrior[second[0]][second[1]].addAll(warrior[i][j]);
+                        }
+                    }
+                }
+            }
+        }
+        warrior = newWarrior;
+        System.out.println(movedWarriorCount + " " + stonedWarriorCount + " " + attackedWarriorCount);
+    }
+
+    static int[] secondMove(int x, int y){
+        int upx = x-1;
+        int downx = x + 1;
+        int lefty = y - 1;
+        int righty = y + 1;
+
+        int[][] dist = new int[4][3];
+        dist[2] = new int[]{upx, y, Math.abs(sr - upx) + Math.abs(sc - y)};
+        dist[3] = new int[]{downx, y, Math.abs(sr - downx) + Math.abs(sc - y)};
+        dist[0] = new int[]{x, lefty, Math.abs(sr - x) + Math.abs(sc - lefty)};
+        dist[1] = new int[]{x, righty, Math.abs(sr - x) + Math.abs(sc - righty)};
+
+        int far = -1;
+        int[] toward = new int[3];
+        for(int i = 0; i<4; i++){
+            if(isValid2(dist[i][0], dist[i][1]) && (far == -1 || far > dist[i][2])){
+                far = dist[i][2];
+                toward = dist[i];
+            }
+        }
+
+        if(far == -1){
+            return new int[]{x, y, 0};
+        }
+        return toward;
+    }
+
+    static int[] firstMove(int x, int y){
+        int upx = x-1;
+        int downx = x + 1;
+        int lefty = y - 1;
+        int righty = y + 1;
+
+        int[][] dist = new int[4][3];
+        dist[0] = new int[]{upx, y, Math.abs(sr - upx) + Math.abs(sc - y)};
+        dist[1] = new int[]{downx, y, Math.abs(sr - downx) + Math.abs(sc - y)};
+        dist[2] = new int[]{x, lefty, Math.abs(sr - x) + Math.abs(sc - lefty)};
+        dist[3] = new int[]{x, righty, Math.abs(sr - x) + Math.abs(sc - righty)};
+
+        int far = -1;
+        int[] toward = new int[3];
+        for(int i = 0; i<4; i++){
+            if(isValid2(dist[i][0], dist[i][1]) && (far == -1 || far > dist[i][2])){
+                far = dist[i][2];
+                toward = dist[i];
+            }
+        }
+
+        if(far == -1){
+            return new int[]{x, y, 0};
+        }
+        return toward;
+    }
+
+    static boolean isValid2(int x, int y){
+        if(x < 0 || x >= n || y < 0 || y >= n){
+            return false;
+        }
+
+        if(stonedArea[x][y]){
+            return false;
+        }
+
+        return true;
+    }
+
+    static boolean[][] stonedArea;
+    static HashSet<String> stonedWarrior;
+    static boolean[][] medusaSeeU, medusaSeeD, medusaSeeL, medusaSeeR;
     static void medusaLook(){
-        medusaSeeU = new int[n][];
-        medusaSeeU = new int[n][];
-        v
+        medusaSeeU = new boolean[n][];
+        medusaSeeD = new boolean[n][];
+        medusaSeeL = new boolean[n][];
+        medusaSeeR = new boolean[n][];
+        for(int i = 0; i<n; i++){
+            medusaSeeU[i] = new boolean[n];
+            medusaSeeD[i] = new boolean[n];
+            medusaSeeL[i] = new boolean[n];
+            medusaSeeR[i] = new boolean[n];
+        }
+
         ArrayList<Warrior> lookU, lookD, lookL, lookR;
         ArrayList<Warrior> choosed = new ArrayList<>();
+        boolean[][] medusaSee = new boolean[n][];
 
         lookU = lookUp();
         lookD = lookDown();
@@ -124,21 +264,36 @@ public class Main {
 
         int count = uCount;
         choosed = lookU;
+        medusaSee = medusaSeeU;
 
         if(count < dCount){
             count = dCount;
             choosed = lookD;
+            medusaSee = medusaSeeD;
         }
         if(count < lCount){
             count = lCount;
             choosed = lookL;
+            medusaSee = medusaSeeL;
         }
         if(count < rCount){
             count = rCount;
             choosed = lookR;
+            medusaSee = medusaSeeR;
         }
 
+        /*
+        for(int i = 0; i<n; i++){
+            System.out.println(Arrays.toString(medusaSeeR[i]));
+        }
         System.out.println(count + ": " + choosed);
+        */
+        stonedArea = medusaSee;
+        stonedWarrior = new HashSet<>();
+        for(Warrior w : choosed){
+            stonedWarrior.add(w.x + " " + w.y);
+        }
+
     }
 
     static ArrayList<Warrior> lookRight(){
@@ -147,6 +302,7 @@ public class Main {
         // sr, sc 
         //왼
         for(int j = sc + 1; j < n; j++){
+            medusaSeeR[sr][j] = true;
             if(warrior[sr][j].size() > 0){
                 looked.add(warrior[sr][j].get(0));
                 break;
@@ -164,7 +320,7 @@ public class Main {
                     wy++;
                     break;
                 }
-
+                medusaSeeR[i][j] = true;
                 //System.out.println(i + ", " + j);
 
                 if(warrior[i][j].size() > 0){
@@ -190,6 +346,7 @@ public class Main {
                     break;
                 }
 
+                medusaSeeR[i][j] = true;
                // System.out.println(i + ", " + j);
 
                 if(warrior[i][j].size() > 0){
@@ -213,6 +370,7 @@ public class Main {
         // sr, sc 
         //왼
         for(int j = sc - 1; j>= 0; j--){
+            medusaSeeL[sr][j] = true;
             if(warrior[sr][j].size() > 0){
                 looked.add(warrior[sr][j].get(0));
                 break;
@@ -231,6 +389,7 @@ public class Main {
                     break;
                 }
 
+                medusaSeeL[i][j] = true;
                 //System.out.println(i + ", " + j);
 
                 if(warrior[i][j].size() > 0){
@@ -255,7 +414,7 @@ public class Main {
                     wy--;
                     break;
                 }
-
+                medusaSeeL[i][j] = true;
                 //System.out.println(i + ", " + j);
 
                 if(warrior[i][j].size() > 0){
@@ -279,6 +438,7 @@ public class Main {
         // sr, sc 
         //위
         for(int i = sr+1 ; i < n; i++){
+            medusaSeeD[i][sc] = true;
             if(warrior[i][sc].size() > 0){
                 looked.add(warrior[i][sc].get(0));
                 break;
@@ -297,6 +457,7 @@ public class Main {
                     break;
                 }
 
+                medusaSeeD[i][j] = true;
                 //System.out.println(i + ", " + j);
 
                 if(warrior[i][j].size() > 0){
@@ -321,6 +482,7 @@ public class Main {
                     break;
                 }
 
+                medusaSeeD[i][j] = true;
                 //System.out.println(i + ", " + j);
 
                 if(warrior[i][j].size() > 0){
@@ -344,6 +506,7 @@ public class Main {
         // sr, sc 
         //위
         for(int i = sr-1 ; i>= 0; i--){
+            medusaSeeU[i][sc] = true;
             if(warrior[i][sc].size() > 0){
                 looked.add(warrior[i][sc].get(0));
                 break;
@@ -362,6 +525,7 @@ public class Main {
                     break;
                 }
 
+                medusaSeeU[i][j] = true;
                 //System.out.println(i + ", " + j);
 
                 if(warrior[i][j].size() > 0){
@@ -385,6 +549,7 @@ public class Main {
                     break;
                 }
 
+                medusaSeeU[i][j] = true;
                 //System.out.println(i + ", " + j);
 
                 if(warrior[i][j].size() > 0){
